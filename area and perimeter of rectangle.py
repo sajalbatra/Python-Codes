c8bbbe8c31b48aa2cfa4ebad7412ec0ca18017dd
Sajal Batra <125984550+sajalbatra@users.noreply.github.com>
2023-03-27 19:15:22 +0530

    Add files via upload

---
+++ area and perimeter of rectangle.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the length and width
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Display the results
+print("The area of the rectangle is:", area)
+print("The perimeter of the rectangle is:", perimeter)
