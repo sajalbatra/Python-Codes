5b495e2b54a9d75431c76f05f6fc28a267590723
Sajal Batra <125984550+sajalbatra@users.noreply.github.com>
2023-04-13 21:02:37 +0530

    Add files via upload

---
+++ maximum of two numbers.py
@@ -0,0 +1,8 @@
+# maximum of two numbers 
+a = int(input("enter the number 1"))
+b = int(input("enter the number 2"))
+if a>b:
+    print("the maximum no, is",a)
+else:
+    print("the maximum no, is",b)
+    
